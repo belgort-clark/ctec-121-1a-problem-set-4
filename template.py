2402af06f6883b34dc349f8ae1cc780c5cd1ac4b
Bruce Elgort <belgort@clark.edu>
2021-10-16 10:16:32 -0700

    Initial commit

---
+++ template.py
@@ -0,0 +1,15 @@
+"""
+CTEC 121
+Date: mm/dd/yyyy
+Your Name
+Assignment Name: Problem Set 4
+Description: Problem X
+"""
+
+
+def main():
+    # your code MUST go here. Be sure to remove this line before you submit your work.
+    pass  # remove this line before you submit
+
+
+main()
